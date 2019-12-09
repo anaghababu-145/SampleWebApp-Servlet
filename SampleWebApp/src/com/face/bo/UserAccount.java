b73f2d523263784776aa606c1a589a2c7cb42c8e
anaghababu-145 <51952265+anaghababu-145@users.noreply.github.com>
2019-12-09 04:30:49 -0800

    Update UserAccount.java

--- SampleWebApp/src/com/face/bo/UserAccount.java
+++ SampleWebApp/src/com/face/bo/UserAccount.java
@@ -1,29 +1,44 @@
+//creating a business object class
+
 package com.face.bo;
+
+//create a class UserAccount
 
 public class UserAccount 
 {
-	public static final String GENDER_MALE ="M";
+    //creating two static final variables
+    public static final String GENDER_MALE ="M";
     public static final String GENDER_FEMALE = "F";
      
+    //creating variables
     private String userName;
-    public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	private String gender;
+    private String gender;
     private String password;
+	
+    //setting getters and setters for variables	
+    public String getUserName() 
+    {
+	return userName;
+    }
+    public void setUserName(String userName) 
+    {
+	this.userName = userName;
+    }
+    public String getGender() 
+    {
+	return gender;
+    }
+    public void setGender(String gender) 
+    {
+	this.gender = gender;
+    }
+    public String getPassword() 
+    {
+	return password;
+    }
+    public void setPassword(String password) 
+    {
+	this.password = password;
+    }
+	
 }
