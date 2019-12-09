b17c95fe178d2cffc8b87c8b99b690deaf0d4563
anaghababu-145 <51952265+anaghababu-145@users.noreply.github.com>
2019-12-09 04:24:45 -0800

    Update ProductAccount.java

--- SampleWebApp/src/com/face/bo/ProductAccount.java
+++ SampleWebApp/src/com/face/bo/ProductAccount.java
@@ -1,33 +1,53 @@
+//program starts here
+//creating a package as com.face.bo
+
 package com.face.bo;
+
+//create a class ProductAccount
 
 public class ProductAccount 
 {
- private String productId;
- public String getProductId() {
-	return productId;
+        //declaring variables as private
+	
+        private String productId;
+        private String productName;
+        private String productQuantity;
+        private String prodcutPrice;
+	
+        //setting getters and setters for the variables
+	
+	public String getProductId() 
+	{
+		return productId;
+	}
+	public void setProductId(String productId) 
+	{
+		this.productId = productId;
+	}
+	public String getProductName() 
+	{
+		return productName;
+	}
+	public void setProductName(String productName) 
+	{
+		this.productName = productName;
+	}
+	public String getProductQuantity() 
+	{
+		return productQuantity;
+	}
+	public void setProductQuantity(String productQuantity) 
+	{
+		this.productQuantity = productQuantity;
+	}
+	public String getProdcutPrice() 
+	{
+		return prodcutPrice;
+	}
+	public void setProdcutPrice(String prodcutPrice) 
+	{
+		this.prodcutPrice = prodcutPrice;
+        }
+
 }
-public void setProductId(String productId) {
-	this.productId = productId;
-}
-public String getProductName() {
-	return productName;
-}
-public void setProductName(String productName) {
-	this.productName = productName;
-}
-public String getProductQuantity() {
-	return productQuantity;
-}
-public void setProductQuantity(String productQuantity) {
-	this.productQuantity = productQuantity;
-}
-public String getProdcutPrice() {
-	return prodcutPrice;
-}
-public void setProdcutPrice(String prodcutPrice) {
-	this.prodcutPrice = prodcutPrice;
-}
-private String productName;
- private String productQuantity;
- private String prodcutPrice;
-}
+//program ends here
